b91b3761304ba1cff9efccdcad36c191e194d215
Zabro <zabrodin.alexander51@gmail.com>
2024-12-20 10:43:03 +0300

    change

--- src/main/java/com/Calculator/Calculator/Controller.java
+++ src/main/java/com/Calculator/Calculator/Controller.java
@@ -1,38 +1,41 @@
 package com.Calculator.Calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class Controller {
-    private Service service = new Service();
+    private final Service service;
 
-    @GetMapping("/calculator")
+    public Controller(Service service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "/")
     public String hello() {
         return service.hello();
     }
 
-    @GetMapping("/calculator/plus")
+    @GetMapping(path = "/plus")
     public String sum(@RequestParam int num1, @RequestParam int num2) {
-        int result = num1 + num2;
         return service.sum(num1, num2);
     }
 
-    @GetMapping("/calculator/minus")
+    @GetMapping(path = "/minus")
     public String minus(@RequestParam int num1, @RequestParam int num2) {
-        int result = num1 - num2;
         return service.minus(num1, num2);
     }
 
-    @GetMapping("/calculator/multiply")
+    @GetMapping(path = "/multiply")
     public String multiply(@RequestParam int num1, @RequestParam int num2) {
-        int result = num1 * num2;
         return service.multyply(num1, num2);
     }
-    @GetMapping("/calculator/divide")
+
+    @GetMapping(path = "/divide")
     public String divide(@RequestParam int num1, @RequestParam int num2) {
-        int result = num1 / num2;
         return service.divide(num1, num2);
     }
 }
--- src/main/java/com/Calculator/Calculator/Service.java
+++ src/main/java/com/Calculator/Calculator/Service.java
@@ -21,9 +21,13 @@ public class Service {
         int result = num1 * num2;
         return String.format("%d * %d = %d", num1, num2, result);
     }
+
     public String divide(int num1, int num2) {
-        int result = num1 / num2;
-        return String.format("%d / %d = %d", num1, num2, result);
+        if (num2 == 0) {
+            return "На ноль делить нельзя!";
+        }
+        double result = num1 / num2;
+        return num1 + "/" + num2 + "=" + result;
     }
 }
 
